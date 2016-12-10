2bc7b7a94c6edbd533c571d9143ef27b9d35d0e5
YWZFrances <1595642024@qq.com>
2016-12-10 21:15:39 +0800

    yidongduan xiangmu

---
+++ dist/js/lianjie.js
@@ -0,0 +1,46 @@
+
+
+
+$(function(){
+	
+	$(".lianjiedaogouwuche").on("click",function(){
+		window.location = "/shopCart.html"
+	})
+	$(".lianjiedaoliebiao").on("click",function(){
+		window.location = "/classify.html"
+	})
+	$(".lianjiedaowode").on("click",function(){
+		window.location = "/mymbs.html"
+	})
+	$(".lianjiedaozhuye").on("click",function(){
+		window.location = "/test.html"
+	})
+	$(".lianjiedaodizhi").on("click",function(){
+		window.location = "/Orders.html"
+	})
+	$(".lianjiedaoshouhuo").on("click",function(){
+		window.location = "/address.html"
+	})
+	
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
